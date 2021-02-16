cf210d6999a4807b9fe9e45ccf4e552683cbf304
Drew Williamson <dreww@vt.edu>
2021-02-15 22:38:39 -0500

    first deploy

--- src/app/team-section/team-section.component.ts
+++ src/app/team-section/team-section.component.ts
@@ -8,12 +8,15 @@ import { Member } from './member';
 })
 export class TeamSectionComponent implements OnInit {
 
-  drewDescription = 'Andrew joined the team in April 2020 in the midst of a global pandemic to help create' +
-    'something new.  He is about to graduate Virginia Tech Cum Laude with a BS in Computer Science and' +
-    'a minor in Business.  Before OSI, he has enjoyed a plethora of experience working with both mobile' +
-    'apps and web platforms.  Through independent studies at University, he wrote 2 native apps for ' +
-    'Android and iOS using Kotlin and Swift.  He also has experience with Angular from his internship with' +
-    'Booz Allen\'s District Defend team in their Strategic Innovation Group';
+  drewDescription = 'Andrew joined the team in April 2020 in the midst of a global pandemic to help create ' +
+    'something new.  He is about to graduate Virginia Tech Cum Laude with a BS in Computer Science and ' +
+    'a minor in Business.  Before OSI, he has enjoyed a plethora of experience working with both mobile ' +
+    'apps and web platforms.  Through independent studies at Virginia Tech, he wrote 2 native apps for ' +
+    'Android and iOS using Kotlin and Swift and an angular web application.  He also has experience with ' +
+    'Angular from his internship with' +
+    ' Booz Allen\'s District Defend team in their Strategic Innovation Group.  In addition to his technical ' +
+    'background, Andrew also demonstrated effective leadership as the first LGBTQ president of his ' +
+    'fraternity, Zeta Psi';
   team = [
     {name: 'Andrew Williamson', title: 'CTO', description: this.drewDescription, imageRef: 'assets/drew.jpeg'}
     ];
